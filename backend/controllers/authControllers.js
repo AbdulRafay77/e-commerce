43b80da6021439dd1d4a90f418fa9de31b62304e
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-09-02 21:11:56 +0500

    build controllers (signup and login)

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,72 @@
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const RefreshToken = require('../models/RefreshToken.js');
+const User = require('../models/User');
+
+const generateTokens = async (user) => {
+  const accessToken = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: '15m' }
+  );
+
+  const rawToken = crypto.randomBytes(40).toString('hex');
+
+  await RefreshToken.create({
+    token: rawToken,
+    userId: user._id,
+    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+  });
+
+  return { accessToken, RefreshToken: rawToken };
+}
+
+const signup = async (req, res) => {
+  try{
+    const { username, email, password } = req.body;
+
+    const user = await User.create({ username, email, password });
+    
+    const { accessToken, refreshToken } = await generateTokens(user);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    res.status(201).json({ accessToken, user: { id: user._id, username: user.username, role: user.role } });
+
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+}
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if(!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    res.status(200).json({ accessToken, user: { id: user._id, username: user.username, role: user.role } })
+
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+}
