a790cd5794f47048f72cccc5e183ebf576fbb202
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-09-02 22:50:17 +0500

    build authMiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  
+  const authHeader = req.headers.authorization;
+
+  if ( !authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Header does not exists.' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    req.user = decoded
+    next();
+
+  } catch (err) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = auth;
