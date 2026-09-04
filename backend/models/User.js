58ef522aaa0e676597ef37dc5eab38f0e7e4f51b
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-09-05 01:13:59 +0500

    fixed next() error

--- backend/models/User.js
+++ backend/models/User.js
@@ -28,11 +28,10 @@ userSchema.methods.comparePassword = function (plainPassword) {
   return bcrypt.compare(plainPassword, this.password);
 };
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 const User = mongoose.model('User', userSchema);
