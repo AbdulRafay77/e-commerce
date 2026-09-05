b0fc882dfbcc340be6185210c83cbf83711e2808
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-09-06 01:53:31 +0500

    set up main.jsx+added the new refresh header system in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -18,4 +18,27 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await api.post('/auth/refresh');
+        setAccessToken(res.data.accessToken);
+        originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
+        return api(originalRequest);
+      } catch (refreshErr) {
+        setAccessToken(null);
+        return Promise.reject(refreshErr);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
