ee85ee5c771d0877bb3c2e8add087c905af66001
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-09-06 02:34:54 +0500

    setup Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+// components/Navbar.jsx
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const Navbar = () => {
+  const { user, logout } = useAuth();
+
+  return (
+    <nav>
+      <Link to="/products">Products</Link>
+      {user ? (
+        <>
+          <span>Welcome, {user.username}</span>
+          <button onClick={logout}>Log Out</button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Log In</Link>
+          <Link to="/signup">Sign Up</Link>
+        </>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
