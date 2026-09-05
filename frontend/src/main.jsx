10baf69d0f8e417e6aa0eda77cc69768415efa9d
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-09-06 02:16:14 +0500

    imported app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
-import { AuthProvider } from './context/AuthContext.jsx';
 import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './context/AuthContext.jsx';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
